3a62a04e22b164dc6d2637af1dadf3661c6ea709
Mofil <mofila@amazon.com>
2025-08-20 09:54:18 +0530

    Create aura.meta.js

---
+++ aura.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Aura Test
+// @namespace    Paragon_Microsites_NPT_SESU_1.0
+// @version      1.7
+// @description  Paragon New AUX widget for Microsites
+// @author       mofila@
+// @include      /^https:\/\/paragon-(na|eu|fe|cn|na-preprod|eu-preprod|fe-preprod)\.amazon\.com\/hz\/(lobby(\/v2)?|.*case.*|dox-search.*|search)$/
+// @updateURL    https://raw.githubusercontent.com/Mofi-l/Aura-Test/main/aura.meta.js
+// @downloadURL  https://raw.githubusercontent.com/Mofi-l/Aura-Test/main/aura.user.js
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
